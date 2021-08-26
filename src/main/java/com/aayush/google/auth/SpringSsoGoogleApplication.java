a1690b6be5d3667f1c1d6dd2128b4bf7c0bd040a
Aayush Agrawal <aayagraw@adobe.com>
2021-08-26 09:48:55 +0530

    first commit

---
+++ src/main/java/com/aayush/google/auth/SpringSsoGoogleApplication.java
@@ -0,0 +1,31 @@
+package com.aayush.google.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class SpringSsoGoogleApplication {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome To Google!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username: " + principal.getName());
+        return principal;
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsoGoogleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aayush/google/auth/SpringSsoGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aayush.google.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoGoogleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
